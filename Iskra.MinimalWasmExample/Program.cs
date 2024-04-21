7008e8367510b6fbf1d761cfbf7461c58b2fd9f0
Miroljub <miroljub1995@gmail.com>
2024-04-21 20:56:41 +0200

    Update

--- Iskra.MinimalWasmExample/Program.cs
+++ Iskra.MinimalWasmExample/Program.cs
@@ -7,23 +7,6 @@ public static class Program
 {
     static async Task Main(string[] args)
     {
-        // string res = await new HttpClient().GetStringAsync("https://api.mydesigns.io/healthz");
-        // Console.WriteLine($"res: {res}");
-        //
-        // string fetchType = JSHost.GlobalThis.GetTypeOfProperty("fetch");
-        // Console.WriteLine($"Fetch type: {fetchType}");
-
-// System.Runtime.InteropServices.JavaScript.JSFunctionBinding.InvokeJS
-
-        // JSHost.GlobalThis.GetPropertyAsJSObject("console").GetPropertyAsJSObject("log").AsJSFunction()
-        //     .Call("this is test and should work", () => { return 1; });
-
-        // JSHost.GlobalThis.GetPropertyAsJSObject("console").GetPropertyAsJSFunction("log").Call("some testtttt");
-
-
-        Console.WriteLine("Is good contructor ");
-
-
         HtmlInputElement element = new Window(JSHost.GlobalThis)
                                        .Document
                                        .GetElementById<HtmlInputElement>("test-input")
@@ -38,23 +21,6 @@ public static class Program
                 Console.WriteLine($"Got event from input with value '{input.Value}'");
             }
         });
-
-        // JSHost.GlobalThis
-        //     .GetPropertyAsJSObject("console")
-        //     .GetPropertyAsJSFunction("log")
-        //     .Call("found input", input);
-        //
-        // Action<object?> cb = (e) =>
-        //     Console.WriteLine(
-        //         $"Got event in c#: {((JSObject)e)
-        //             .GetPropertyAsJSObject("target")
-        //             .GetPropertyAsString("value")}");
-        //
-        // JSObject jsCb = cb.ToJSObject();
-        //
-        // input
-        //     .GetPropertyAsJSFunction("addEventListener")
-        //     .Call("input", jsCb);
 
         Console.WriteLine("Hello, World!");
     }
