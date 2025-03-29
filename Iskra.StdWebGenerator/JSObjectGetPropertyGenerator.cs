5b6447352ec59b1b6934966c4dd0bcaa5cd027e5
Miroljub <miroljub1995@gmail.com>
2025-03-29 14:39:34 +0100

    Fix OneOf

--- Iskra.StdWebGenerator/JSObjectGetPropertyGenerator.cs
+++ Iskra.StdWebGenerator/JSObjectGetPropertyGenerator.cs
@@ -14,54 +14,60 @@ public static class JSObjectGetPropertyGenerator
     {
         var asNullableSuffix = type.IsNullable ? "AsNullable" : "";
 
-        MyType returnType = type switch
+        return type switch
         {
-            _ when type.Type.IsArray => new MyType(
-                Type: typeof(JSObject),
-                IsNullable: type.IsNullable,
-                ElementType: null,
-                GenericTypeArguments: []
+            _ when type.Type.IsArray => new(
+                Name: $"GetPropertyAsAsJSObjectV2{asNullableSuffix}",
+                ReturnType: new MyType(
+                    Type: typeof(JSObject),
+                    IsNullable: type.IsNullable,
+                    ElementType: null,
+                    GenericTypeArguments: []
+                )
             ),
-            _ when type.Type == typeof(bool) => type,
-            _ when type.Type == typeof(bool?) => type,
-            _ when type.Type == typeof(int) => type,
-            _ when type.Type == typeof(int?) => type,
-            _ when type.Type == typeof(long) => type,
-            _ when type.Type == typeof(long?) => type,
-            _ when type.Type == typeof(double) => type,
-            _ when type.Type == typeof(double?) => type,
-            _ when type.Type == typeof(double) => type,
-            _ when type.Type == typeof(string) => type,
-            _ when type.Type == typeof(JSObject) => type,
-            _ when type.Type.IsOneOf() => new MyType(
-                Type: typeof(JSObject),
-                IsNullable: type.IsNullable,
-                ElementType: null,
-                GenericTypeArguments: []
+            _ when type.Type == typeof(bool) => new(
+                Name: $"GetPropertyAsBoolean{asNullableSuffix}",
+                ReturnType: type
             ),
-            _ when type.Type.IsJSObjectWrapper() => new MyType(
-                Type: typeof(JSObject),
-                IsNullable: type.IsNullable,
-                ElementType: null,
-                GenericTypeArguments: []
+            _ when type.Type == typeof(int) => new(
+                Name: $"GetPropertyAsInt32{asNullableSuffix}",
+                ReturnType: type
+            ),
+            _ when type.Type == typeof(long) => new(
+                Name: $"GetPropertyAsInt64{asNullableSuffix}",
+                ReturnType: type
+            ),
+            _ when type.Type == typeof(double) => new(
+                Name: $"GetPropertyAsDouble{asNullableSuffix}",
+                ReturnType: type
+            ),
+            _ when type.Type == typeof(string) => new(
+                Name: $"GetPropertyAsStringV2{asNullableSuffix}",
+                ReturnType: type
+            ),
+            _ when type.Type == typeof(JSObject) => new(
+                Name: $"GetPropertyAsJSObjectV2{asNullableSuffix}",
+                ReturnType: type
+            ),
+            _ when type.Type.IsOneOf() => new(
+                Name: $"GetPropertyAsOneOf{asNullableSuffix}",
+                ReturnType: new MyType(
+                    Type: typeof(JSObject),
+                    IsNullable: type.IsNullable,
+                    ElementType: null,
+                    GenericTypeArguments: []
+                )
+            ),
+            _ when type.Type.IsJSObjectWrapper() => new(
+                Name: $"GetPropertyAsJSObjectV2{asNullableSuffix}",
+                ReturnType: new MyType(
+                    Type: typeof(JSObject),
+                    IsNullable: type.IsNullable,
+                    ElementType: null,
+                    GenericTypeArguments: []
+                )
             ),
             _ => throw new($"Property of type {type} is not supported."),
         };
-
-        string asType = returnType switch
-        {
-            _ when returnType.Type == typeof(bool) => "AsBoolean",
-            _ when returnType.Type == typeof(int) => "AsInt32",
-            _ when returnType.Type == typeof(long) => "AsInt64",
-            _ when returnType.Type == typeof(double) => "AsDouble",
-            _ when returnType.Type == typeof(string) => "AsStringV2",
-            _ when returnType.Type == typeof(JSObject) => "AsJSObjectV2",
-            _ => throw new($"Property of type {type} is not supported."),
-        };
-
-        return new(
-            Name: $"GetProperty{asType}{asNullableSuffix}",
-            ReturnType: returnType
-        );
     }
 }
